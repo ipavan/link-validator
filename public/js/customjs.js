4a8b9390dd6dd18a7d68ffb6bf62bbdf62ab07b1
Pavan Sadarangani <psadarangani@psadaranga-ltm4.internal.salesforce.com>
2019-08-29 10:43:52 +0100

    updates

---
+++ public/js/customjs.js
@@ -0,0 +1,207 @@
+$(document).ready(function(){
+
+	startUp();
+	let emailAssetData = [];
+	let deObjectId = [];
+	let externalDEKeys = [];
+	let subscriberKeys = [];
+	let emails = [];
+	let emailsLoaded = false
+	let desLoaded = false
+
+	function startUp() {
+		$('#links').hide();
+		$('#emailSelect').hide();
+		$('#deSelect').hide();
+		$('#updateButton').hide();
+		$.ajax({
+	        type: "GET",
+	        url: "/getEmails",
+	        success: function(res) {
+	        	emailsLoaded = true;
+	        	showPicklists();
+	         	emailAssetData = res.body.items;
+	         	console.log(res.body.items);
+	         	if (res.body.items) {
+		         	let output = `
+					 	<div id="emailReplace">
+						  <label class="slds-form-element__label" for="select-01">Select Email</label>
+						  <div class="slds-form-element__control">
+						    <div class="slds-select_container">
+						      <select class="slds-select" id="emails">
+						        <option value="Unselected">Please Select an Email to Validate</option>`;
+				let count = 0;
+	         	emailAssetData.forEach(function(item) {
+	         		let name = item['name'];
+	         		emails.push(item['legacyData']['legacyId']);
+	         		output += `<option value=${count}>${name}</option>`;
+	         		count+=1;
+	         	});
+	         	output += `</select>
+							    </div>
+							  </div>
+							</div>`;
+
+
+					$('#emailReplace').replaceWith(output);
+				}
+        	}
+        });
+
+        $.ajax({
+        	type: "GET",
+        	url: "/getDEs",
+        	success: function(res) {
+        		desLoaded = true;
+        		console.log(res);
+        		showPicklists();
+        		if (res['Results']) {
+	        		let output = `
+					 	<div id="deReplace">
+						  <label class="slds-form-element__label" for="select-01">Select Data Extension</label>
+						  <div class="slds-form-element__control">
+						    <div class="slds-select_container">
+						      <select class="slds-select" id="denames">
+						        <option value="Unselected">Please Select a Data Extension to test against</option>`;
+					let count = 0;
+	        		res['Results'].forEach(function(item) {
+	        			console.log(item);
+	        			output += `<option value=${count}>${item['Name'][0]}</option>`;
+	        			externalDEKeys.push(item['CustomerKey'][0]);
+	        			subscriberKeys.push(item['SendableDataExtensionField'][0]['Name'][0]);
+	        			deObjectId.push(item['ObjectID'][0]);
+	        			count+=1;
+	        		});
+	        		output += `</select>
+								    </div>
+								  </div>
+								</div>`;
+
+
+					$('#deReplace').replaceWith(output);
+					$('#denames').change(function() {
+						$('#updateButton').hide();
+						getDataExtensionSubs();
+					});
+				}
+        	}
+        });
+	}
+
+	function showPicklists() {
+		if (emailsLoaded && desLoaded) {
+			$('#spinna').hide();
+	        $('#deSelect').show();
+	        $('#emailSelect').show();
+		}
+	}
+
+	function getDataExtensionSubs() {
+		$('#spinna').show();
+		let externalKey = externalDEKeys[$('#denames').val()];
+		let sendableField = subscriberKeys[$('#denames').val()];
+		$.ajax({
+			type: "POST",
+			url: "/getSubs",
+			data: {deKey: externalKey, sendableKey: sendableField},
+			success: function(res) {
+				$('#spinna').hide();
+				$('#updateButton').show();
+				console.log(res);
+				if (res['Results']) {
+					let output = `
+					 	<div id="subscribersList">
+						  <label class="slds-form-element__label" for="select-01">Select Subscriber</label>
+						  <div class="slds-form-element__control">
+						    <div class="slds-select_container">
+						      <select class="slds-select" id="subscriberIds">
+						        <option value="Unselected">Select a Subscriber to validate against</option>`;
+					let count = 0;
+					res['Results'].forEach(function(item) {
+						let userId = item['Properties'][0]['Property'][0]['Value'][0];
+						output += `<option value="${userId}">${userId}</option>`;
+						count+=1;
+					});
+					output += `</select>
+								    </div>
+								  </div>
+								</div>`;
+
+
+					$('#subscribersList').replaceWith(output);
+				}
+			}
+		})
+	}
+
+	$("#validateButton").click(function(){
+		$('#spinna').show();
+        console.log("get sub preview");
+        let row = $('#subscriberIds').val();
+        let emailId = emails[$('#emails').val()];
+        let deId = deObjectId[$('#denames').val()];
+
+        $.ajax({
+        	type: "POST",
+        	url: "/getSubscriberPreview",
+        	data: {row: row, emailId: emailId, deId: deId},
+        	success: function(res) {
+        		$('#spinna').hide();
+        		console.log(res);
+        		let views = res.body.message.views;
+        		views.forEach(function(item) {
+        			if (item.contentType.includes('textBody')) {
+        				console.log('Got the htmlbody');
+        				extractLinks(item.content);
+        			}
+        		});
+        	}
+        });
+    });
+
+    function extractLinks(html) {
+    	let output=`<table class="slds-table slds-table_cell-buffer slds-table_bordered slds-table_striped" id="links">
+		  <thead>
+		    <tr class="slds-line-height_reset">
+		      <th class="" scope="col">
+		        <div class="slds-truncate" title="Opportunity Name">Base URL</div>
+		      </th>
+		      <th class="" scope="col">
+		        <div class="slds-truncate" title="Account Name">Path</div>
+		      </th>
+		      <th class="" scope="col">
+		        <div class="slds-truncate" title="Close Date">Valid</div>
+		      </th>
+		    </tr>
+		    <tbody>`;
+
+    	console.log(html);
+    	let regex = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
+    	let results;
+    	while ((results = regex.exec(html)) != null) {
+    		console.log(results);
+    		let base = results[1];
+    		let path = '';
+    		if (results[4]) {
+    			path = results[4];
+    		}
+    		output+=`<tr class="slds-hint-parent">
+		      <th data-label="Opportunity Name" scope="row">
+		        <div class="slds-truncate" title="Base URL">${base}</div>
+		      </th>
+		      <td data-label="Account Name">
+		        <div class="slds-truncate" title="Sample Path">${path}</div>
+		      </td>
+		      <td data-label="Close Date">
+		        <div class="slds-truncate" title="True">True</div>
+		      </td>
+		    </tr>`
+    	}
+
+    	output+=`</tbody></thead>
+		</table>`;
+
+		$('#links').replaceWith(output);
+    }
+});
+
--- server.js
+++ server.js
@@ -1,10 +1,192 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+const request = require('request-promise');
+var parseString = require('xml2js').parseString;
 var app = express();
 var port = process.env.PORT || 8080;
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 // Serve your app
 console.log('Served: http://localhost:' + port);
 app.listen(port);
+
+global.marketingCloudAccessToken = {
+	access_token: "",
+	expiry: null,
+	isExpired: true
+}
+
+function getAccessToken() {
+	return new Promise((resolve, reject) => {
+		if (global.marketingCloudAccessToken.expiry) {
+			console.log('Ive gotten a token before');
+			let timediff = global.marketingCloudAccessToken.expiry - process.hrtime()[0];
+			if (timediff > 20) {
+				console.log('my token is still active');
+				global.marketingCloudAccessToken.isExpired = false;
+			} else {
+				console.log('my token is expired');
+				global.marketingCloudAccessToken.isExpired = true;
+			}
+		}
+		if (global.marketingCloudAccessToken.isExpired == true) {
+			console.log('getting new token');
+			var options = {
+	            method: 'POST',
+	            uri: `https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.auth.marketingcloudapis.com/v2/token`,
+	            body: {
+	              'grant_type': 'client_credentials',
+	              'client_id': `0yx4e8t1xqsegm9ur7heq9tm`,
+	              'client_secret': `Q4QiUd1WxFAPlwR2LGzxXnKK`,
+	              'account_id':'500009370'
+	            },
+	            json: true
+	        }
+			request(options, (err, res) => {
+				if (err) reject(err);
+				else {
+					global.marketingCloudAccessToken.access_token = res['body']['access_token'];
+					global.marketingCloudAccessToken.expiry = res['body']['expires_in'] ? process.hrtime()[0] + res['body']['expires_in'] : null;
+					global.marketingCloudAccessToken.isExpired = false;
+					console.log(global.marketingCloudAccessToken.access_token);
+					resolve(global.marketingCloudAccessToken.access_token);
+				};
+			})
+		}
+		else {
+			resolve(global.marketingCloudAccessToken.access_token);
+		}
+	})
+}
+
+function getEmails() {
+	return new Promise((resolve, reject) => {
+		var options = {
+            method: 'POST',
+            uri: `https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.rest.marketingcloudapis.com/asset/v1/content/assets/query`,
+            headers: {
+			    'Content-Type': 'application/json',
+			    'Authorization': `Bearer ${global.marketingCloudAccessToken.access_token}`
+			},
+            body: {
+              'page': {
+              	'page':1,
+              	'pageSize':50
+              },
+              'query':{
+              	'leftOperand': {
+              		'property':'assetType.name',
+              		'simpleOperator': 'equal',
+              		'value': 'templatebasedemail'
+              	},
+              	'logicalOperator': 'OR',
+              	'rightOperand': {
+              		'property': 'assetType.name',
+              		'simpleOperator':'equal',
+              		'value':'htmlemail'
+              	}
+              }
+            },
+            json: true
+        }
+        request(options, (err, res) => {
+        	if (err) reject(err);
+        	resolve(res);
+        });
+	})
+}
+
+app.get('/getEmails', (req, res) => {
+	getAccessToken().then((data) => {
+		getEmails().then((result) => {
+			res.send(result);
+		});
+	});
+});
+
+app.get('/getDEs', (req, res) => {
+	getAccessToken()
+	.then((data) => {
+		const xml = `<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">    <s:Header>        <a:Action s:mustUnderstand="1">Retrieve</a:Action>        <a:To s:mustUnderstand="1">https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.soap.marketingcloudapis.com/Service.asmx</a:To>        <fueloauth xmlns="http://exacttarget.com">${global.marketingCloudAccessToken.access_token}</fueloauth>    </s:Header>    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">      <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">         <RetrieveRequest>            <ObjectType>DataExtension</ObjectType>            <Properties>ObjectID</Properties>            <Properties>CustomerKey</Properties>       <Properties>SendableDataExtensionField.Name</Properties>  <Properties>SendableSubscriberField.Name</Properties>   <Properties>Name</Properties>            <Filter xsi:type="SimpleFilterPart">                    <Property>IsSendable</Property>                    <SimpleOperator>equals</SimpleOperator>                    <Value>True</Value>            </Filter>         </RetrieveRequest>      </RetrieveRequestMsg>    </s:Body></s:Envelope>`;
+		var options = {
+		    method: 'POST',
+		    uri: `https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.soap.marketingcloudapis.com/Service.asmx`,
+		    headers: {
+		        'Content-Type': 'text/xml'
+		    },
+		    body: xml,
+		    json: false 
+		};
+		request(options, (err, response) => {
+			if (err) {
+				console.log(err);
+				reject(err);
+			}
+			else {
+				parseString(response['body'], function (err, result) {
+					let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
+					res.send(envelope);
+				});
+			}
+		});
+	});
+});
+
+app.post('/getSubs', (req, res) => {
+	let deKey = req.body.deKey;
+	let sendableField = req.body.sendableKey;
+	getAccessToken()
+	.then((data) => {
+		let xml = `<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">    <s:Header>        <a:Action s:mustUnderstand="1">Retrieve</a:Action>        <a:To s:mustUnderstand="1">https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.soap.marketingcloudapis.com/Service.asmx</a:To>        <fueloauth xmlns="http://exacttarget.com">${global.marketingCloudAccessToken.access_token}</fueloauth>    </s:Header>    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">            <RetrieveRequest>                        <ObjectType>DataExtensionObject[${deKey}]</ObjectType>                        <Properties>${sendableField}</Properties>                     </RetrieveRequest>        </RetrieveRequestMsg>    </s:Body></s:Envelope>`;
+		var options = {
+		    method: 'POST',
+		    uri: `https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.soap.marketingcloudapis.com/Service.asmx`,
+		    headers: {
+		        'Content-Type': 'text/xml'
+		    },
+		    body: xml,
+		    json: false 
+		};
+		request(options, (err, response) => {
+			if (err) {
+				console.log(err);
+				reject(err);
+			}
+			else {
+				parseString(response['body'], function(err, result) {
+					let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
+					res.send(envelope);
+				})
+			}
+		});
+	});
+});
+
+app.post('/getSubscriberPreview', (req, res) => {
+	let row = req.body.row;
+	let emailId = req.body.emailId;
+	let deId = req.body.deId;
+	getAccessToken()
+	.then((data) => {
+		var options = {
+            method: 'POST',
+            uri: `https://mcmn2cc-hkrhh8yfwb0v0mz8y2q4.rest.marketingcloudapis.com/guide/v1/emails/${emailId}/dataExtension/${deId}/contacts/key:${row}/preview`,
+            headers: {
+			    'Content-Type': 'application/json',
+			    'Authorization': `Bearer ${global.marketingCloudAccessToken.access_token}`
+			},
+            json: true
+        }
+
+        console.log(options.uri);
+
+        request(options, (err, result) => {
+        	if (err) reject(err);
+        	res.send(result);
+        });
+	});
+});
